7e32ba26648f2cd08307151242f3f0f920f074f9
Mateusz <mateusz.karpowicz@outlook.com>
2020-11-10 17:35:25 +0100

    [BW-04] Adds an add favourite component and currency component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 // COMPONENTS
 import Navigation from './components/Navigation';
-import Landing from './components/Landing';
+// import Landing from './components/Landing';
+import AddFavourite from './components/AddFavourite';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <div className="container flex">
-        <Landing />
+      <div className="container flex flex-ai-c flex-jc-c">
+        {/* <Landing /> */}
+        <AddFavourite />
       </div>
     </div>
   );
---
+++ src/components/AddFavourite.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+// COMPONENTS
+import Currency from './Currency';
+
+// IMPORT MOCKUP DATA
+import currenciesArray from '../helpers/mockupData';
+
+function AddFavourite() {
+  // eslint-disable-next-line
+  const [currencies, setCurrencies] = useState(currenciesArray);
+  const [displayCurrencies, setDisplayCurrencies] = useState(currencies);
+
+  const search = (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+    const filteredCurrencies = currencies.filter((el) => {
+      return (
+        el.currency.toLowerCase().indexOf(searchQuery) !== -1 ||
+        el.code.toLowerCase().indexOf(searchQuery) !== -1
+      );
+    });
+    return setDisplayCurrencies(filteredCurrencies);
+  };
+
+  return (
+    <div className="add-favourite">
+      <input
+        type="'text"
+        className="search-input"
+        placeholder="search for currency"
+        onChange={(e) => search(e)}
+      />
+      <button type="button" className="btn back-btn">
+        go back
+      </button>
+      <div className="currency-container flex flex-jc-c">
+        {displayCurrencies.map(({ currency, mid, code }) => {
+          return (
+            <Currency
+              currency={currency}
+              mid={mid}
+              code={code}
+              key={currency.code}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default AddFavourite;
